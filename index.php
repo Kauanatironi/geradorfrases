cd3a4f1ffe97af52323116cec7f69eaf535514f0
Kauanatironi <kauanaleticia81@gmail.com>
2017-04-27 14:22:50 -0300

    adicionando parte dinâmica do algoritmo

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
+<?php
+
+$titulos = ["Senhor", "Espetacular", "Supremo"];
+$qualidades = ["puxador de cobertas", "abridor de geladeiras", "alegria mundial"];
+$nome = "feriado";
+
+?>
+
 <html>
 <head>
-    
+
 </head>
 
 <body>
 
-<h2>oi oi</h2>
+<h2>hi <?php echo $titulos[rand(0,2)]. "$nome, ".$qualidades[rand(0,2)];?> </h2>
 
 </body>
 
